09a9ee5224d4cc4b2e4f5d0e846023806b2a5fc1
Nick Berezny <nickberezny@hotmail.com>
2024-01-31 15:32:40 -0500

    client sends gonio

--- src/Client.c
+++ src/Client.c
@@ -51,15 +51,15 @@ void * clientThread (void * d)
             //sprintf(buffer, "PLOT::%.2f::%.2f", s_client->x, s_client->x0);
             if(controlParams->x0_is_percent)
             {
-                sprintf(buffer, "PLOT::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::", s_client->x/controlParams->xend, 
+                sprintf(buffer, "PLOT::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2F::", s_client->x/controlParams->xend, 
                     s_client->x0_to_send,s_client->x0_duration,s_client->Fext,controlParams->Kest,
-                    s_client->emg1,s_client->emg2,s_client->emg3,s_client->emg4);
+                    s_client->emg1,s_client->emg2,s_client->emg3,s_client->emg4, s_client->gonio);
             }
             else
             {
-                sprintf(buffer, "PLOT::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::", s_client->x/controlParams->xend, s_client->x0_to_send/controlParams->xend,
+                sprintf(buffer, "PLOT::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2F::", s_client->x/controlParams->xend, s_client->x0_to_send/controlParams->xend,
                     s_client->x0_duration,s_client->Fext, s_client->Fext,controlParams->Kest,
-                    s_client->emg1,s_client->emg2,s_client->emg3,s_client->emg4); 
+                    s_client->emg1,s_client->emg2,s_client->emg3,s_client->emg4, s_client->gonio); 
                     
                // double xtest = 1.0;
                 //sprintf(buffer, "PLOT::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::%.2f::",xtest,xtest + 1.0,xtest + 2.0,xtest+3.0,xtest+4.0,xtest,xtest,xtest,xtest);
