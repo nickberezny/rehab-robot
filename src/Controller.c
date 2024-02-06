7ca1ead66852c7f655b9d6053176730208580d7d
Nick Berezny <nickberezny@hotmail.com>
2024-02-06 12:18:40 -0500

    update

--- src/Controller.c
+++ src/Controller.c
@@ -174,9 +174,10 @@ void * controllerThread (void * d)
                 //run adm for delta (reset)
                 break;
             case FORCE_NORM_MODE:
-                s->x0
-                s->x0_to_send = 0.5*s->xend;
+                s->x0 = 0.5 + (0.1/controlParams->xend);
+                s->x0_to_send = 0.5*controlParams->xend;
                 AdmittanceMode(s, controlParams);
+                break;
                 
             
         }
