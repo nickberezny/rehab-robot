d2f6d7d81a201d66a0b014c8490225dc8bf6cf43
Nick Berezny <nickberezny@hotmail.com>
2024-02-28 15:37:08 -0500

    updated i2c

--- src/I2C_Test.c
+++ src/I2C_Test.c
@@ -23,7 +23,7 @@ void I2C(int handle)
 	const char * I2C_READ_NAME = "I2C_DATA_RX";
 
 	int numBytes, errAdress;
-	char aBytes[32] = {0}; // TX/RX bytes will go here
+	char aBytes[32] = {0x6B, 0x00}; // TX/RX bytes will go here
 
 	LJM_eWriteName(handle, "I2C_SDA_DIONUM", 0);
 	LJM_eWriteName(handle, "I2C_SCL_DIONUM", 1);
@@ -36,17 +36,11 @@ void I2C(int handle)
 	LJM_eWriteName(handle, "I2C_NUM_BYTES_RX", 6); // Set the number of bytes to receive
 
 	// Set the TX bytes. We are sending 1 byte for the address.
-	numBytes = 1;
-	aBytes[0] = 0x6B; // Byte 0: Memory pointer = 0
+	numBytes = 2;
+	//aBytes[0] = {0x6B; // Byte 0: Memory pointer = 0
 	LJM_eWriteNameByteArray(handle, I2C_WRITE_NAME, numBytes, aBytes, &errAdress);
 	LJM_eWriteName(handle, "I2C_GO", 1); // Do the I2C communications.
 
-	sleep(1);
-
-	numBytes = 1;
-	aBytes[0] = 0; // Byte 0: Memory pointer = 0
-	LJM_eWriteNameByteArray(handle, I2C_WRITE_NAME, numBytes, aBytes, &errAdress);
-	LJM_eWriteName(handle, "I2C_GO", 1); // Do the I2C communications.
 
 	sleep(1);
 
