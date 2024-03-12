3f1c4cf1041e3dcf20adb3a69d0ff165974c3cc2
Nick Berezny <nickberezny@hotmail.com>
2024-03-12 12:37:21 -0400

    changed x0 on adm dist

--- src/Controller.c
+++ src/Controller.c
@@ -195,7 +195,7 @@ void * controllerThread (void * d)
                     AdmittanceZeroStiffnessMode(s, controlParams);
                     //PositionMode(s, controlParams);
                 }
-                s->x0_to_send = controlParams->x0;
+                s->x0_to_send = controlParams->x0*controlParams->xend;
                 //AdmittanceMode(s, controlParams);
                 break;
             
